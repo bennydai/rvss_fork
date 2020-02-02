e1db445ae4150d5ec52d4bdb0dff4804d5b33761
Pieter van Goor <pieter.vangoor@anu.edu.au>
2020-02-03 10:36:13 +1100

    Fix minor syntax error

--- network/ComputeAnimalBearings.py
+++ network/ComputeAnimalBearings.py
@@ -11,31 +11,31 @@ class PosedImage:
         self.img_name = img_dict["imgfname"]
         
 
-def write_bearings(self, neuralnet, bearings_file, folder_name=""):
-    # Obtain neural net output
-    img = Image.open(folder_name+self.img_name)
-    heatmap = neuralnet.sliding_window(img)
+    def write_bearings(self, neuralnet, bearings_file, folder_name=""):
+        # Obtain neural net output
+        img = Image.open(folder_name+self.img_name)
+        heatmap = neuralnet.sliding_window(img)
 
-    # Compute animal bearings here and save to self.animals.
-    # Next, you can use all this information to triangulate the animals!
-    
-    bearings = {}
-    # For example, finding the llamas:
-    # if np.any(heatmap == 2.0):
-    #     llama_coords = np.where(heatmap == 2.0)
-    #     average_llama = np.mean(llama_coords, axis=1)
-    #     bearings["llama"] = ...
-    # Now you need to convert this to a horizontal bearing as an angle.
-    # Use the camera matrix for this!
+        # Compute animal bearings here and save to self.animals.
+        # Next, you can use all this information to triangulate the animals!
+        
+        bearings = {}
+        # For example, finding the llamas:
+        # if np.any(heatmap == 2.0):
+        #     llama_coords = np.where(heatmap == 2.0)
+        #     average_llama = np.mean(llama_coords, axis=1)
+        #     bearings["llama"] = ...
+        # Now you need to convert this to a horizontal bearing as an angle.
+        # Use the camera matrix for this!
 
-    # There are ways to get much better bearings.
-    # Try and think of better solutions than just averaging.
+        # There are ways to get much better bearings.
+        # Try and think of better solutions than just averaging.
 
-    for animal in bearings:
-        bearing_dict = {"pose":self.pose.tolist(),
-                        "animal":animal,
-                        "bearing":bearings[animal]}
-        bearing_line = json.dumps(bearing_dict)
+        for animal in bearings:
+            bearing_dict = {"pose":self.pose.tolist(),
+                            "animal":animal,
+                            "bearing":bearings[animal]}
+            bearing_line = json.dumps(bearing_dict)
 
 
 if __name__ == "__main__":
