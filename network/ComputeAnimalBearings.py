62404b8bd2d5706e4353930ff5f1001b4403aa0a
Pieter van Goor <pieter.vangoor@anu.edu.au>
2020-02-03 10:33:25 +1100

    Rewrite triangulate to compute animal bearings

---
+++ network/ComputeAnimalBearings.py
@@ -0,0 +1,56 @@
+import evaluate
+from PIL import Image
+import json
+import numpy as np
+
+
+class PosedImage:
+    def __init__(self, json_line):
+        img_dict = json.loads(json_line)
+        self.pose = np.array(img_dict["pose"])
+        self.img_name = img_dict["imgfname"]
+        
+
+def write_bearings(self, neuralnet, bearings_file, folder_name=""):
+    # Obtain neural net output
+    img = Image.open(folder_name+self.img_name)
+    heatmap = neuralnet.sliding_window(img)
+
+    # Compute animal bearings here and save to self.animals.
+    # Next, you can use all this information to triangulate the animals!
+    
+    bearings = {}
+    # For example, finding the llamas:
+    # if np.any(heatmap == 2.0):
+    #     llama_coords = np.where(heatmap == 2.0)
+    #     average_llama = np.mean(llama_coords, axis=1)
+    #     bearings["llama"] = ...
+    # Now you need to convert this to a horizontal bearing as an angle.
+    # Use the camera matrix for this!
+
+    # There are ways to get much better bearings.
+    # Try and think of better solutions than just averaging.
+
+    for animal in bearings:
+        bearing_dict = {"pose":self.pose.tolist(),
+                        "animal":animal,
+                        "bearing":bearings[animal]}
+        bearing_line = json.dumps(bearing_dict)
+
+
+if __name__ == "__main__":
+    # Set up the network
+    exp = evaluate.Evaluate()
+
+    # Read in the images
+    images_fname = "../system_output/images.txt"
+    with open(images_fname, 'r') as images_file:
+        posed_images = [PosedImage(line) for line in images_file]
+
+    # Compute bearings and write to file
+    bearings_fname = "../system_output/bearings.txt"
+    with open(bearings_fname, 'w') as bearings_file:
+        for posed_image in posed_images:
+            posed_image.write_bearings(exp, bearings_file, "../system_output/")
+
+    
