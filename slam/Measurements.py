59f44edb6d1f0cba83a1e3ee8fa8a5b3e2e305f8
sbeinke <scott.k.beinke@gmail.com>
2020-02-06 20:43:38 +1100

    added better covariance

--- slam/Measurements.py
+++ slam/Measurements.py
@@ -5,7 +5,24 @@ class MarkerMeasurement:
     def __init__(self, position, tag, covariance = (0.1*np.eye(2))):
         self.position = position
         self.tag = tag
-        self.covariance = covariance
+        #self.covariance=covariance
+        self.covariance = self.covariance_matrix_calculation(position)
+
+    def covariance_matrix_calculation(self, lm_bff2d, sigma_u=0.05, sigma_rho_param = 0.2):
+        """ New method to initialise the covariance. Turning the x,y representation from aruco into a
+            (polar) estimate of d,phi, assumed independent. Estimate sigma_rho by depth of marker
+            Put this into aruco_detector.py and call in lm_measurements for the parameter covariance
+        """
+        # new covariance initialisation
+        dist_est = np.sqrt(lm_bff2d[0]**2 + lm_bff2d[1]**2)
+        u = np.divide(lm_bff2d,dist_est)
+        u_mat = u @ u.T
+
+        # rho = 1 /(dist_est)       # we never actually need rho
+        cov_u = (sigma_u**2) * (np.eye(2) - u_mat)
+        cov_rho = sigma_rho_param*(dist_est**2) * u_mat
+        cov_full = cov_u + cov_rho
+        return cov_full
 
 class DriveMeasurement:
     # Measurement of the robot wheel velocities
