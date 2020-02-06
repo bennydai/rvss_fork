f0456eb210fa9b8af805fd098d43f85ac28f9e2f
sbeinke <scott.k.beinke@gmail.com>
2020-02-07 00:38:10 +1100

    Fuse Results script

--- Triangulate.py
+++ Triangulate.py
@@ -2,17 +2,18 @@ import numpy as np
 
 def eta(alpha):
     return np.mat([
-        [-np.sin(alpha)], 
+        [-np.sin(alpha)],
         [np.cos(alpha)]
     ])
 
 def rpk(theta):
     return np.mat([
-        [np.cos(theta), -np.sin(theta)], 
+        [np.cos(theta), -np.sin(theta)],
         [np.sin(theta),  np.cos(theta)]
     ])
 
 def triangulate(meas):
+    #Measurement is of the format list of [x,y,theta,alpha]
     n = np.shape(meas)[0]
     xpk   = meas[:,0:2]
     theta = meas[:,2]
@@ -31,4 +32,3 @@ if __name__ == '__main__':
         [2, 0, np.radians(179), 0]
     ])
     print(triangulate(meas))
-
---
+++ data_fusion/FuseResults.py
@@ -0,0 +1,76 @@
+import sys
+import os
+import json
+import numpy as np
+sys.path.insert(0, "../")
+sys.path.insert(0, "../slam")
+from SlamMap import SlamMap
+from Triangulate import triangulate
+
+def loadDataset(dataDir,slamFile='slam.txt',bearingFile='bearings.txt'):
+
+    #load slam map
+    slamFilePath=dataDir + '/' + slamFile
+    slammap=SlamMap()
+    slammap.load(slamFilePath)
+
+    print(slammap.markers[0])
+
+    #load bearings
+    with open('../system_output/bearings.txt') as f:
+       bearing_dict = json.load(f)
+
+    ConvertBearingDict(bearing_dict)
+
+    print(bearing_dict[0])
+
+    return slammap, bearing_dict
+
+def ConvertBearingDict(bearing_dict):
+    for bearing in bearing_dict:
+        bearing['pose']=np.array(bearing['pose']).reshape(3,1)
+
+def ConvertBearing(bearing,R,t):
+        homoT = np.zeros((3,3))
+        homoT[0][0:2] = R[0][0:2]
+        homoT[1][0:2] = R[1][0:2]
+        homoT[0][2] = t[0]
+        homoT[1][2] = t[1]
+        homoT[2][2] = 1
+        Pose=bearing['pose']
+        theta=Pose[2]
+        PoseRepMat=np.mat([
+            [np.cos(theta), -np.sin(theta), Pose[0]],
+            [np.sin(theta),  np.cos(theta), Pose[1]],
+            [0,0,1]
+        ])
+
+        NewPose=homoT @ PoseRepMat
+        x=NewPose[0,2]
+        y=NewPose[1,2]
+        thetanew=np.arccos(NewPose[0,0])
+        bearing['pose']=np.array([x,y,thetanew]).reshape(3,1)
+        #bearing['pose']= t + bearing['pose']
+        #bearing['theta']= R
+        print(bearing['pose'])
+
+if __name__ == '__main__':
+
+    dataDir='../system_output'
+    dataDir2='../someone_elses_system_output'
+    slammap, bearing_dict = loadDataset(dataDir)
+
+    slammap2, bearing_dict2 = loadDataset(dataDir2)
+
+    armse, R, t=slammap.compute_tf(slammap2)
+
+    for bearing in bearing_dict2:
+        ConvertBearing(bearing,R,t)
+
+    bearing_dict=bearing_dict# ++ bearing_dict2
+    print(bearing_dict)
+
+    for animal in ("elephant", "crocodile","llama","snake"):
+        bearings = [x for x in bearing_dict if x["animal"] == animal]
+        meas = [detection["pose"].hstack()(detection["bearing"]) for detection in bearings]
+        print(triangulate(meas))
