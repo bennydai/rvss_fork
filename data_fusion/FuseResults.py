cc1ea1886b64440facc5462d06108f216b639f09
sbeinke <scott.k.beinke@gmail.com>
2020-02-07 02:11:26 +1100

    Added plot output to FuseResults

--- data_fusion/FuseResults.py
+++ data_fusion/FuseResults.py
@@ -6,6 +6,7 @@ sys.path.insert(0, "../")
 sys.path.insert(0, "../slam")
 from SlamMap import SlamMap
 from Triangulate import triangulate
+import matplotlib.pyplot as plt
 
 def loadDataset(dataDir,slamFile='slam.txt',bearingFile='bearings.txt'):
 
@@ -14,15 +15,11 @@ def loadDataset(dataDir,slamFile='slam.txt',bearingFile='bearings.txt'):
     slammap=SlamMap()
     slammap.load(slamFilePath)
 
-    print(slammap.markers[0])
-
     #load bearings
     with open('../system_output/bearings.txt') as f:
        bearing_dict = json.load(f)
 
     ConvertBearingDict(bearing_dict)
-
-    print(bearing_dict[0])
 
     return slammap, bearing_dict
 
@@ -31,12 +28,14 @@ def ConvertBearingDict(bearing_dict):
         bearing['pose']=np.array(bearing['pose']).reshape(3,1)
 
 def ConvertBearing(bearing,R,t):
+        #Calculate homogenous transformation matrix
         homoT = np.zeros((3,3))
         homoT[0][0:2] = R[0][0:2]
         homoT[1][0:2] = R[1][0:2]
         homoT[0][2] = t[0]
         homoT[1][2] = t[1]
         homoT[2][2] = 1
+        #assemble pose matrix
         Pose=bearing['pose']
         theta=Pose[2]
         PoseRepMat=np.mat([
@@ -50,9 +49,7 @@ def ConvertBearing(bearing,R,t):
         y=NewPose[1,2]
         thetanew=np.arccos(NewPose[0,0])
         bearing['pose']=np.array([x,y,thetanew]).reshape(3,1)
-        #bearing['pose']= t + bearing['pose']
-        #bearing['theta']= R
-        print(bearing['pose'])
+        #print(bearing['pose'])
 
 def combineBearingDicts(slammap1,bearing_dict1,slammap2,bearing_dict2):
     #Converts and fuses bearing_dict2 into slammap1 frame and appends converted bearing_dict2 resutls to bearing_dict1
@@ -62,7 +59,6 @@ def combineBearingDicts(slammap1,bearing_dict1,slammap2,bearing_dict2):
         ConvertBearing(bearing,R,t)
 
     bearing_dict1=bearing_dict1 + bearing_dict2
-    print(bearing_dict1)
 
 if __name__ == '__main__':
     #Assumes results from each bot in a different directory
@@ -79,10 +75,19 @@ if __name__ == '__main__':
         slammap3, bearing_dict3 = loadDataset(dataDir3)
         combineBearingDicts(slammap1,bearing_dict1,slammap3,bearing_dict3)
 
+    fig=plt.figure()
+    #ax=fig.add_axes([0,0,1,1])
+
     for animal in ("elephant", "crocodile","llama","snake"):
         bearings = [x for x in bearing_dict1 if x["animal"] == animal]
         meas = [np.concatenate([detection["pose"].flatten(),np.array([detection["bearing"]])]) for detection in bearings]
         if len(meas) == 0:
             continue
         print('Position of ', animal)
-        print(triangulate(np.array(meas)))
+        ans=triangulate(np.array(meas))
+        print(ans)
+        plt.scatter(ans[0],ans[1],label=animal)
+        plt.legend()
+        plt.grid(True)
+
+plt.show()
